92722230be7e51a14613b9ff91f4dcc4ce998144
Youssefares <Youssefares@ymail.com>
2017-03-23 20:25:26 +0200

    tested is_sorted

--- is_sorted.py
+++ is_sorted.py
@@ -4,12 +4,3 @@ def is_sorted(a:[int], lo:int = 0, hi:int = sentinel):
     if hi is sentinel:
         hi = len(a)-1
     return all([a[i] <= a[i+1] for i in range(lo, hi)])
-
-
-#testing
-assert is_sorted([2,3,3,5])
-assert is_sorted([1])
-assert not is_sorted([2,0])
-assert is_sorted([2,3,4,5,0,234,0], 0, 3)
-assert not is_sorted([2,3,4,5,234,0], 4, 5)
-assert is_sorted([53,2,0], 2, 2)
--- test_sorts.py
+++ test_sorts.py
@@ -17,15 +17,27 @@ class BSTTestCase(unittest.TestCase):
             elif a > self.max:
                 self.max = a
 
-    def test_tree_get_min_is_correct(self):
+    def test_tree_min_is_correct(self):
         result = self.tree.min()
         self.assertEqual(result, self.min)
 
 
-    def test_tree_get_max_is_correct(self):
+    def test_tree_max_is_correct(self):
         result = self.tree.max()
         self.assertEqual(result, self.max)
 
-    
+
+class isSortedTestCase(unittest.TestCase):
+    def setUp(self):
+        self.arr = random.sample(range(-1000,1000),100)
+
+
+    def test_is_sorted_works(self):
+        self.assertFalse(is_sorted(self.arr))
+        self.assertTrue(sorted(self.arr))
+
+
+
+
 
 unittest.main()
