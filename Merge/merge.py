f3adfb9bf81986f8eb1bc868669652902a984e7f
Youssefares <Youssefares@ymail.com>
2017-03-23 20:24:56 +0200

    moved and refactored MergeSort

--- Merge/merge.py
+++ Merge/merge.py
@@ -1,26 +1,3 @@
-from is_sorted import is_sorted
-
-sentinel = object()
-def merge_sort(a:[int], aux:[int]=None, lo:int=0, hi:int=sentinel):
-    #just initializing hi for the 1st callback
-    if hi is sentinel:
-        hi = len(a)-1
-
-    #Base case
-    if hi <= lo:
-        return
-
-    mid = (lo+hi)//2
-    #sort left half
-    merge_sort(a, aux, lo, mid)
-    #sort right half
-    merge_sort(a, aux, mid+1, hi)
-
-    #if things are out of order, merge..
-    #else nothing to be done.
-    if a[mid+1] < a[mid]:
-        merge(a, aux, lo, mid, hi)
-
 #TODO: can we modify this so that it doesn't move so much data arround
 def merge(a:[int], aux:[int], lo:int, mid:int, hi:int):
     assert is_sorted(a, lo, mid)
---
+++ Merge/merge_sort.py
@@ -0,0 +1,22 @@
+from Merge.merge import merge
+
+sentinel = object()
+def merge_sort(a:[int], aux:[int]=None, lo:int=0, hi:int=sentinel):
+    #just initializing hi for the 1st callback
+    if hi is sentinel:
+        hi = len(a)-1
+
+    #Base case
+    if hi <= lo:
+        return
+
+    mid = (lo+hi)//2
+    #sort left half
+    merge_sort(a, aux, lo, mid)
+    #sort right half
+    merge_sort(a, aux, mid+1, hi)
+
+    #if things are out of order, merge..
+    #else nothing to be done.
+    if a[mid+1] < a[mid]:
+        merge(a, aux, lo, mid, hi)
